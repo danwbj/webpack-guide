12cab6310426ad07f156349188f5d04d9614d420
吴丹丹 <danwbj@163.com>
2018-12-13 16:59:52 +0800

    feat:尝试watch自动编译

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,42 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+var ManifestPlugin = require("webpack-manifest-plugin");
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    app: "./src/index.js",
+    print: "./src/print.js"
+  },
+  devtool: "inline-source-map",
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
   },
   mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader?name=img/[name].[hash:8].[ext]"]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader?name=img/[name].[hash:8].[ext]"]
-      }
-    ]
-  }
+  //   module: {
+  //     rules: [
+  //       {
+  //         test: /\.css$/,
+  //         use: ["style-loader", "css-loader"]
+  //       },
+  //       {
+  //         test: /\.(png|svg|jpg|gif)$/,
+  //         use: ["file-loader?name=img/[name].[hash:8].[ext]"]
+  //       },
+  //       {
+  //         test: /\.(woff|woff2|eot|ttf|otf)$/,
+  //         use: ["file-loader?name=img/[name].[hash:8].[ext]"]
+  //       }
+  //     ]
+  //   }
+  devServer: {
+    contentBase: "./dist"
+  },
+  plugins: [
+    new ManifestPlugin(),
+    new CleanWebpackPlugin(["dist"]),
+    new HtmlWebpackPlugin({
+      title: "Output Management"
+    })
+  ]
 };

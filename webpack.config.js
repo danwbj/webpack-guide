65bb4da889ba3ab2ac3c4b9a9dc22a44605cfa09
吴丹丹 <danwbj@163.com>
2018-12-13 15:01:00 +0800

    feat:init and test

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader?name=img/[name].[hash:8].[ext]"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader?name=img/[name].[hash:8].[ext]"]
+      }
+    ]
+  }
+};

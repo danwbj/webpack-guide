c358b40869cb767c7d0249f758821e3b207a458b
吴丹丹 <danwbj@163.com>
2018-12-23 19:49:02 +0800

    feat:配置多个entry实现代码分离会造成多处引用的库重复打包到bundle中，使用splitChunks可以实现防止重复

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+// const webpack = require("webpack");
+const UUID = require("uuid");
 module.exports = {
   entry: {
     index: "./src/index.js",
@@ -27,10 +29,61 @@ module.exports = {
       //   }
     ]
   },
+  optimization: {
+    runtimeChunk: {
+      name: "manifest"
+    },
+    // splitChunks: {
+    //   cacheGroups: {
+    //     commons: {
+    //       test: /[\\/]node_modules[\\/]/,
+    //       name: "vendor",
+    //       chunks: "all"
+    //     }
+    //   }
+    // }
+    splitChunks: {
+      chunks: "all",
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      name(module) {
+        let uuid = UUID.v1()
+          .toString()
+          .split("-");
+        return "vendors." + uuid[0];
+      },
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          chunks: "initial",
+          name: "vendor",
+          priority: 10
+        },
+        common: {
+          chunks: "initial",
+          name: "common",
+          minSize: 0,
+          priority: 2
+        },
+        styles: {
+          name: "styles",
+          test: /\.scss|less|css$/,
+          chunks: "all",
+          enforce: true
+        }
+      }
+    }
+  },
   plugins: [
     new CleanWebpackPlugin(["dist"]),
     new HtmlWebpackPlugin({
       title: "webpack demo"
     })
+    //webpack3的用法，4之后改方法已经移除，使用config.optimization.splitChunks方式替换，见上面配置
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: "common" // 指定公共 bundle 的名称。
+    // })
   ]
 };

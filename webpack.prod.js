e7832662b9e8e56eced70d039e36de09edb5fe7d
吴丹丹 <danwbj@163.com>
2018-12-23 17:21:54 +0800

    feat:生产环境使用source-map方便调试

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,11 @@ const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
+  devtool: "source-map",
   mode: "production",
-  plugins: [new UglifyJSPlugin()]
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true
+    })
+  ]
 });

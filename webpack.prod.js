094843c4842244ac87e0251d9673740ff4c01c18
吴丹丹 <danwbj@163.com>
2018-12-23 18:56:16 +0800

    feat:为所有的依赖定义这个变量，因为许多 library 将通过与 process.env.NODE_ENV 环境变量关联，以决定 library 中应该引用哪些内容

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,16 @@
 const merge = require("webpack-merge");
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const common = require("./webpack.common.js");
-
+const webpack = require("webpack");
 module.exports = merge(common, {
   devtool: "source-map",
   mode: "production",
   plugins: [
     new UglifyJSPlugin({
       sourceMap: true
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production") //为所有的依赖定义这个变量，因为许多 library 将通过与 process.env.NODE_ENV 环境变量关联，以决定 library 中应该引用哪些内容
     })
   ]
 });

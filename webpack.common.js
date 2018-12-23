38b3257b7215fd6d4841b407faca001887e4caf6
吴丹丹 <danwbj@163.com>
2018-12-23 16:42:51 +0800

    feat:分离webpack.config.js为生产环境和开发环境

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+module.exports = {
+  entry: ["./src/index.js"],
+  output: {
+    publicPath: "/",
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+      //   {
+      //     test: /\.(png|svg|jpg|gif)$/,
+      //     use: ["file-loader?name=img/[name].[hash:8].[ext]"]
+      //   },
+      //   {
+      //     test: /\.(woff|woff2|eot|ttf|otf)$/,
+      //     use: ["file-loader?name=img/[name].[hash:8].[ext]"]
+      //   }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    new HtmlWebpackPlugin({
+      title: "Output Management"
+    })
+  ]
+};
